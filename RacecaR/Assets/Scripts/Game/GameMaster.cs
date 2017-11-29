15f6bcce92ea42088742d282a9ddf0ed0d3e19d4
Lucas F <lucasfreitas09876@gmail.com>
2017-11-29 18:44:42 -0500

    added and changed a bunch of stuff, sorry i killed the menu

---
+++ RacecaR/Assets/Scripts/Game/GameMaster.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour {
+
+    public Text timerText;
+    float timer;
+    public bool inversion;
+
+    public PlayerController player;
+    public PlayerController player2;
+    public float inverseDuration = 0;
+    public float inverseCountdown;
+
+    private void Start()
+    {
+        inverseCountdown = Random.Range(15f, 20f);
+        timerText = GameObject.Find("Timer").GetComponent<Text>();
+    }
+    private void Update()
+    {
+        
+        timer = timer + 1 * Time.deltaTime;
+        timerText.text = "" + timer;
+
+        /*if (timer >= 25)
+        {
+            inversion = true;
+        }*/
+
+        if (inverseDuration > 0)
+        {
+            inverseDuration -= 1 * Time.deltaTime;
+        }
+
+        if (inverseCountdown > 0)
+        {
+            inverseCountdown -= 1 * Time.deltaTime;
+        }
+
+        if(inverseCountdown <= 0)
+        {
+            inverseDuration = Random.Range(4f, 10f);
+            player.reverse = true;
+            player2.reverse = true;
+        }
+
+        if(inverseDuration <= 0)
+        {
+            inverseCountdown = Random.Range(15f, 25f);
+            player.reverse = false;
+            player2.reverse = false;
+        }
+
+
+    }
+}
---
+++ RacecaR/Assets/Scripts/Mine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour {
+
+
+    public PlayerController player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player = other.GetComponent<PlayerController>();
+        }
+
+        player.reverse = !player.reverse;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player = other.GetComponent<PlayerController>();
+        }
+
+        player.reverse = player.reverse;
+    }
+}
