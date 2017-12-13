643aaaff407fe17668517f1b21cf69944fed3771
Lucas F <lucasfreitas09876@gmail.com>
2017-12-13 11:04:32 -0500

    added reverse UI

---
+++ RacecaR/Assets/Scripts/UI/Inverse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inverse : MonoBehaviour {
+
+    public GameMaster gm;
+    public Animator anim;
+
+    private void Start()
+    {
+        gm = GameObject.Find("GameMaster").GetComponent<GameMaster>();
+        anim = gameObject.GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        anim.SetBool("isReversed", gm.inverseActive);
+        anim.SetFloat("Countdown", gm.inverseCountdown);
+        anim.SetFloat("Duration", gm.inverseDuration);
+    }
+}
